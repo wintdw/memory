c73636a4a3cba170c8e46201ec15cf37ba75e9e9
Dung Trung Nguyen <dungw@coccoc.com>
2019-06-17 17:40:46 +0700

    oom

---
+++ oom/oom.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ALLOCSZ = 1024*1024*1024*3  // 6G
+
+int main() {
+	// declare memory allocation
+	int *array = malloc(ALLOCSZ);
+	memset(array, 0, ALLOCSZ/2);
+
+	exit(0);
+}
