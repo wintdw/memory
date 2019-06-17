0f3ca679947f05acb5c0a7e7ef62efe340b93adb
Dung Trung Nguyen <dungw@coccoc.com>
2019-06-17 17:58:26 +0700

    change host

--- oom/oom.c
+++ oom/oom.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/mman.h>
 
-#define ALLOCSZ 1024*1024*1024*3l  // 3G
+#define ALLOCSZ 1024*1024*1024*6l  // 6G
 
 int main() {
 	// declare memory allocation
-	int *array = malloc(ALLOCSZ);
-	memset(array, 0, ALLOCSZ/2);
+    int *ptr = mmap(NULL, ALLOCSZ, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_NORESERVE, -1, 0);
+	memset(ptr, 0, ALLOCSZ/4);
 
+    sleep(20000);
 	exit(0);
 }
--- overcommit/overcommit.c
+++ overcommit/overcommit.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/mman.h>
 
 #define ALLOCSZ 1024*1024*1024*6l  // 6G
 
