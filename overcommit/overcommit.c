d889972fe83560751b0806877fb2fbef1d17e08b
Dung Trung Nguyen <dungw@coccoc.com>
2019-06-17 17:49:22 +0700

    map_noreserve

--- overcommit/overcommit.c
+++ overcommit/overcommit.c
@@ -7,8 +7,9 @@
 
 int main() {
 	// declare memory allocation
-	int *array = malloc(ALLOCSZ);
-	memset(array, 0, ALLOCSZ/4);
+    int *ptr = mmap(NULL, ALLOCSZ, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_NORESERVE, -1, 0);
+	memset(ptr, 0, ALLOCSZ/4);
 
+    sleep(20000);
 	exit(0);
 }
