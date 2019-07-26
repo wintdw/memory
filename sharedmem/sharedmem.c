5c17ecebd141507cb4a6ae1589cb3da20a7297a5
Dung Trung Nguyen <dungw@coccoc.com>
2019-07-26 22:58:38 +0700

    update sharemem demo

--- sharedmem/sharedmem.c
+++ sharedmem/sharedmem.c
@@ -11,16 +11,16 @@
 #define ONEG 1024*1024*1024l
 
 int main(int argc, char *argv[]) {
-    int size = ONEG * atol(argv[1]);
-	int *ptr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
-	memset(ptr, 1, MAPSIZE);
+	int fd = open(argv[1], O_RDWR);
+	struct stat sb;
+	fstat(fd, &sb);
+	int *ptr = mmap(NULL, sb.st_size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_FILE, fd, 0);
+	memset(ptr, 1, sb.st_size);
 
 	// Shared mem
 	pid_t child;
 	if ((child = fork()) == 0) {
-		memset(ptr, 2, size);
-		int devNull = open("/dev/null", O_WRONLY);
-		int b = write(devNull, ptr, MAPSIZE);
+		memset(ptr, 2, sb.st_size);
 		int *p = mmap(NULL, ONEG, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 		memset(p, 3, ONEG);
 	}
